fe434f1f30c811b46beebef6957b9a7b4e45ee89
Lauren <laurenelizamax@icloud.com>
2020-02-29 15:33:51 -0600

    initial commit

---
+++ dialogs.js
@@ -0,0 +1,44 @@
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+    
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--bart").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--bart")
+            theDialog.showModal()
+        }
+    ),
+        
+    document.querySelector("#button--betty").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--betty")
+            theDialog.showModal()
+        }
+    ),
+
+    document.querySelector("#button--cindy").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--cindy")
+            theDialog.showModal()
+        }
+    )
+}
+export default initializeDetailButtonEvents
